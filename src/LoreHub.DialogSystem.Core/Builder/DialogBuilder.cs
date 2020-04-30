0dccff28c14d7591e6232952359233eb5c4c912a
Andrey Kniazev <worldpwn@hotmail.com>
2020-04-30 10:30:55 +0300

    Added new methods

--- src/LoreHub.DialogSystem.Core/Builder/DialogBuilder.cs
+++ src/LoreHub.DialogSystem.Core/Builder/DialogBuilder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using LoreHub.DialogSystem.Core.Builder.Models;
+using LoreHub.DialogSystem.Core.DialogOptions;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -25,11 +26,26 @@ namespace LoreHub.DialogSystem.Core.Builder
                 jsonArray = (JArray)JToken.ReadFrom(reader);
             }
 
-            //JsonSerializer jsonSerializer = new JsonSerializer();
-            //jsonSerializer.Converters.Add(_contentJsonConverter);
+            List<NodeModel<TContent>> nodeModels = jsonArray.ToObject<List<NodeModel<TContent>>>();
 
-            List<NodeModel<TContent>> nodes = jsonArray.ToObject<List<NodeModel<TContent>>>();
+            //List<DialogNode<TContent>> nodes = new List<DialogNode<TContent>>();
+            //foreach (NodeModel<TContent> node in nodeModels)
+            //{
+            //    nodes.Add(DialogNode<TContent>.CreateNew)
+            //}
 
+           // Dialog<TContent> dialog = Dialog<TContent>.CreateNew(nodeModels[0]);
+
+            throw new NotImplementedException();
+        }
+
+        public DialogNode<TContent> CreateFromModel(NodeModel<TContent> model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDialogOption<TContent> CreateFromModel(OptionModel<TContent> model)
+        {
             throw new NotImplementedException();
         }
     }
